55836a10255027d79111d28cf1101f8a14dd1872
suru_4851 <35515322+RahulSurana123@users.noreply.github.com>
2024-07-10 12:27:13 +0530

    Create Variable_and_Values.rs
    Learning about mut and different types of assignment and types of variable

---
+++ Variable_and_Values.rs
@@ -0,0 +1,23 @@
+const STARTING_MISSILES: i32  = 8;    
+const READY_AMOUNT: i32 = 2;
+
+fn main() {
+    let missiles = STARTING_MISSILES;
+    let ready = READY_AMOUNT;
+    
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    
+    let mut missiles = missiles;
+    
+    missiles = missiles - ready;
+    
+    println!("{} missiles left", missiles);
+    
+    let (mut missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
+    
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    
+    missiles = missiles - ready;
+    println!("{} missiles left", missiles);
+    
+}
