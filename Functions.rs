ef01f3a68ec2ad33017ba810f3ffc39bd1084867
suru_4851 <35515322+RahulSurana123@users.noreply.github.com>
2024-07-10 14:03:32 +0530

    Create Functions.rs
    Added this after completing the assignment

---
+++ Functions.rs
@@ -0,0 +1,44 @@
+#![allow(unused_variables)]
+
+// This a Assignment from my crash course on Rust by Nathan Stocks
+fn main() {
+    let width = 4;
+    let height = 7;
+    let depth = 10;
+    // 1. Try running this code with `cargo run` and take a look at the error.
+    //
+    // See if you can fix the error. It is right around here, somewhere.  If you succeed, then
+    // doing `cargo run` should succeed and print something out.
+    
+    let area = area_of(width, height);
+    
+    println!("Area is {}", area);
+
+    // 2. The area that was calculated is not correct! Go fix the area_of() function below, then run
+    //    the code again and make sure it worked (you should get an area of 28).
+
+    // 3. Uncomment the line below.  It doesn't work yet because the `volume` function doesn't exist.
+    //    Create the `volume` function!  It should:
+    //    - Take three arguments of type i32
+    //    - Multiply the three arguments together
+    //    - Return the result (which should be 280 when you run the program).
+    //
+    // If you get stuck, remember that this is *very* similar to what `area_of` does.
+    //
+    println!("Volume is {}", volume(width, height, depth));
+}
+
+fn area_of(x: i32, y: i32) -> i32 {
+    // 2a. Fix this function to correctly compute the area of a rectangle given
+    // dimensions x and y by multiplying x and y and returning the result.
+    
+    x * y
+    
+}
+
+fn volume(x: i32, y: i32, z:i32) -> i32{
+    // Created the function according to assignment requirement
+    
+    x*y*z
+
+}
